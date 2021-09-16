0cf3705fb5db564c4552568f599e03b9dadd255f
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-16 14:05:51 -0700

    Final touch
    Removed extra spaces, code and comments. Styling complete

--- scripts.js
+++ scripts.js
@@ -1,51 +1,38 @@
 let dob = document.querySelector("#dob");
 let luckyNum = document.querySelector("#luckyNum");
-let showLucky  = document.querySelector("#showLucky");
+let showLucky = document.querySelector("#showLucky");
 let btnCheck = document.querySelector("#btn-check");
 
-
-showLucky.style.display="none"; 
-
+showLucky.style.display = "none";
 
 
 function divide(someDate, someNum) {
-    let answer=  someDate % someNum; 
-    console.log(answer + " is your answer"); 
-    showLucky.style.display="block";
-    if (!answer) {
-        console.log("Your birthday is lucky");
-        showLucky.innerText= " 🎉 YAY ! Your birthday is lucky 🎁" ;
-    // } else if (answer === NaN) {
-    //     showLucky.innerText = "Please enter a valid input! "
+    showLucky.style.display = "block";
+    if (someNum == 0 || someDate == "") {                 
+        showLucky.innerText = "🚫 Please enter a valid input!"      
     } else {
-        console.log("your birtday is not lucky");
-        showLucky.innerText = "Aww... Your birthday is not that lucky for this number";
-    }
+        let answer = someDate % someNum;       
+        
+        if (!answer) {            
+            showLucky.innerText = " 🎉 YAY ! Your birthday is lucky 🎁";
+        } else {            
+            showLucky.innerText = "Aww... Your birthday is not that lucky for this number 🤝";
+        }
+    }    
 }
-
 
 function getDigits(dateString) {
-    let date = dateString.replaceAll("-", ""); 
-    console.log(date);
-
-    let total = 0; 
-    for (let i=0; i<date.length; i++) {
-        total = total + Number(date.charAt(i));       
-    }  
-    console.log(`Your total is ${total}`); 
-    return total;    
+    let date = dateString.replaceAll("-", "");    
+    let total = 0;
+    for (let i = 0; i < date.length; i++) {
+        total = total + Number(date.charAt(i));
+    }
+    return total;
 }
 
-
-function checkLucky() {
-    
-    console.log( "You entered " + luckyNum.value + " as number & " + dob.value ); 
-    let  addedDate = getDigits(dob.value);  
-    divide(addedDate, luckyNum.value);
+function checkLucky() {      
+    let addedDate = getDigits(dob.value);
+    divide(addedDate, luckyNum.value);    
 }
-    
-
-
 
 btnCheck.addEventListener("click", checkLucky);
-
